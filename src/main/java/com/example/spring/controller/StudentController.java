c6a7b717aadffe22cae56ecfbe0c3299eb78dff0
FitiavanaLova <hei.fitiavana@gmail.com>
2026-03-30 15:32:11 +0300

    RequestHeader

--- src/main/java/com/example/spring/controller/StudentController.java
+++ src/main/java/com/example/spring/controller/StudentController.java
@@ -1,10 +1,7 @@
 package com.example.spring.controller;
 
 import com.example.spring.model.Student;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,6 +17,20 @@ public class StudentController {
         return studentList.stream()
                 .map(s -> s.getFirstName() + " " + s.getLastName())
                 .collect(Collectors.joining(", "));
+    }
+
+    public String getStudents(@RequestHeader(value = "Accept", defaultValue = "text/plain") String acceptHeader) {
+
+        if ("text/plain".equals(acceptHeader) || "*/*".equals(acceptHeader)) {
+            if (studentList.isEmpty()) {
+                return "Aucun étudiant enregistré.";
+            }
+            return studentList.stream()
+                    .map(s -> s.getFirstName() + " " + s.getLastName())
+                    .collect(Collectors.joining(", "));
+        } else {
+            return "Format non supporté";
+        }
     }
 
 }
